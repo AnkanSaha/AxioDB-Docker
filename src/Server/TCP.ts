91d72e0a5c66a3035b719dc2da0708d356e232cd
Ankan Saha <ankansahadaluabari@gmail.com>
2025-04-14 02:05:03 +0530

    feat: implement Fastify and TCP server setup with routing and logging

---
+++ src/Server/TCP.ts
@@ -0,0 +1,29 @@
+// Import TCP Server Configuration
+import net from "node:net";
+import { Port } from "../config/Keys";
+const PORT: number = Number(Port.TCP) || 27018;
+
+const server = net.createServer((socket) => {
+    console.log('Client connected');
+
+    socket.on('data', (data) => {
+        console.log('Received from client:', data.toString());
+        socket.write(`Echo: ${data}`);
+    });
+
+    socket.on('end', () => {
+        console.log('Client disconnected');
+    });
+
+    socket.on('error', (err) => {
+        console.error('Socket Error:', err.message);
+    });
+});
+
+const tcpServer = async () => {
+    server.listen(PORT, () => {
+        console.log(`TCP Server listening on port ${PORT}`);
+    });
+}
+
+export default tcpServer;
