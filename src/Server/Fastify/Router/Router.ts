e351b41f2398e614a351494f950d180f631bfad0
Ankan Saha <ankansahadaluabari@gmail.com>
2025-04-17 22:32:23 +0530

    refactor: update bcrypt implementation and dependencies
    - Changed bcrypt dependency to bcryptjs for better compatibility.
    - Updated nodemon configuration by removing unnecessary event logs.
    - Bumped package version from 1.2.7 to 1.3.8 in package-lock.json.
    - Refactored Authentication class to improve password hashing logic.
    - Cleaned up package-lock.json by removing deprecated and unused packages.
    - Updated Router.ts to enhance code clarity.

--- src/Server/Fastify/Router/Router.ts
+++ src/Server/Fastify/Router/Router.ts
@@ -1,6 +1,7 @@
 import { FastifyInstance } from "fastify";
 import userAuthentication from "./Authentication/Authentication";
 
+// Main Service Routes to link all the routes to the main service
 export default function MainServiceRoutes(
   fastify: FastifyInstance,
   options: any,
